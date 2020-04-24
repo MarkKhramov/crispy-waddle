38caeca47d352d2ea84bda7c92de2ec1c37be616
Mark K <markkhramov@outlook.com>
2020-04-24 15:03:49 -0600

    Finished basic operator-button behaviors

--- calculator.js
+++ calculator.js
@@ -1,16 +1,19 @@
-
+let x = undefined;
+let y = undefined;
+let operator = undefined;
+let monitorValue = "";
 function add(x,y){
-    return x + y;
+    return Number(x) + Number(y);
 }
 function subtract(x,y){
-    return x - y;
+    return Number(x) -Number(y);
 }
 
 function multiply(x,y){
-    return x*y;
+    return Number(x)*Number(y);
 }
 function divide(x,y){
-    return x/y;
+    return Number(x)/Number(y);
 }
 function operate(x,y,operator){
     switch(operator){
@@ -20,7 +23,7 @@ function operate(x,y,operator){
         case '-':
             return subtract(x,y);
             break;
-        case '*':
+        case 'x':
             return multiply(x,y);
             break;
         case '/':
@@ -30,14 +33,44 @@ function operate(x,y,operator){
             return undefined;
 	}
 }
-function keyBehavior(e){
-	console.log(this.textContent);
-	monitor.value += this.textContent;
+function responseNumeralClick(){
+	monitorValue += this.textContent;
+	monitor.value = monitorValue;
 }
 
-const keyboard = Array.from(document.querySelectorAll(".num"));
+function responseOperationClick(){
+	if(operator === undefined){	
+		x = monitorValue;
+		operator = this.id;
+		monitorValue = "";
+	}
+}
+function clear()
+{
+	monitorValue = "";
+	monitor.value = "";
+	x = undefined;
+	y = undefined;
+	operator = undefined;
+}
+function performOperation(){
+	if(monitorValue != ""&& operator != undefined){
+		y = monitorValue;
+		monitorValue = operate(x,y,operator);
+		operator = undefined;
+		y = undefined;
+		monitor.value = monitorValue;
+	}
+}
+const numPad = Array.from(document.querySelectorAll(".num"));
+const operatorPad =Array.from(document.querySelectorAll(".op"));
 const monitor = document.getElementById("monitor");
 const negateInput = document.getElementById("negate");
+const clearButton =document.getElementById("clear");
+const equalButton = document.getElementById("equals");
 
-keyboard.forEach(key => key.addEventListener("click", keyBehavior));
+operatorPad.forEach(key => key.addEventListener("click", responseOperationClick));
+numPad.forEach(key => key.addEventListener("click", responseNumeralClick));
 negateInput.addEventListener("click", () => monitor.value *= -1);
+clearButton.addEventListener("click", clear);
+equalButton.addEventListener("click", performOperation);
