b1a84e04a0b071684782da4b85ca076cb889fc78
Mark K <markkhramov@outlook.com>
2020-05-07 17:08:06 -0600

    Finished squashing bugs

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@ let y = undefined;
 let operator = undefined;
 let monitorValue = "";
 function add(x,y){
+      
     return Number(x) + Number(y);
 }
 function subtract(x,y){
@@ -27,14 +28,28 @@ function operate(x,y,operator){
             return multiply(x,y);
             break;
         case '/':
-            return divide(x,y);
-            break;
+            if(y != 0){
+		    return divide(x,y);
+	    }
+	    else{
+		    monitorValue = "0";
+		    return "Impossible";
+	    }
+	    break;
         default:
             return undefined;
 	}
 }
 function responseNumeralClick(){
-	monitorValue += this.textContent;
+        if(monitor.value == "Impossible"){
+		monitorValue = this.textContent;
+	}
+	else if(monitor.value != "0"){
+		monitorValue += this.textContent;
+	}
+	else{
+		monitorValue = this.textContent;
+	}
 	monitor.value = monitorValue;
 }
 
@@ -48,7 +63,7 @@ function responseOperationClick(){
 function clear()
 {
 	monitorValue = "";
-	monitor.value = "";
+	monitor.value = "0";
 	x = undefined;
 	y = undefined;
 	operator = undefined;
@@ -62,15 +77,30 @@ function performOperation(){
 		monitor.value = monitorValue;
 	}
 }
+function checkDotValidity(){
+	if(monitorValue.search(".")==-1)
+	{
+		monitorValue += this.textContent;
+		monitor.value = monitorValue;
+	}
+}
+
 const numPad = Array.from(document.querySelectorAll(".num"));
 const operatorPad =Array.from(document.querySelectorAll(".op"));
 const monitor = document.getElementById("monitor");
 const negateInput = document.getElementById("negate");
 const clearButton =document.getElementById("clear");
 const equalButton = document.getElementById("equals");
+const dotButton = document.getElementById("dot");
 
 operatorPad.forEach(key => key.addEventListener("click", responseOperationClick));
 numPad.forEach(key => key.addEventListener("click", responseNumeralClick));
-negateInput.addEventListener("click", () => monitor.value *= -1);
+negateInput.addEventListener("click", function(){ 
+		monitor.value *=-1;
+		monitorValue *= -1;
+	}
+);
 clearButton.addEventListener("click", clear);
 equalButton.addEventListener("click", performOperation);
+dotButton.addEventListener("click", checkDotValidity);
+
